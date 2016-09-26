30a780f0d8b74ebdec7dee1e568e81c772e05392
Eric Denton <eric@iamericanartist.com>
2016-09-26 15:10:10 -0500

    basic AngularJS setup and working

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const express = require("express")                                    //pull Exp
 const app = express()                                                 //initialize Express
 const PORT = process.env.PORT || 3000                                 //set up ports
 
-
 app.use(express.static('client'))                                     //express base directory is the ROOT, not the folder where the server is - established in package.JSON
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))     //server/server.js console.log()
