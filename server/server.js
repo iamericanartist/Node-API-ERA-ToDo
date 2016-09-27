a50597a8072f1028ee687052c772b98867ac0905
Eric Denton <eric@iamericanartist.com>
2016-09-26 21:13:46 -0500

    mongoose installed promises made...

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 "use strict"
 
 const express = require("express")                                    //pull Express in
+const mongoose = require('mongoose');                                 //pull Mongoose in
 
 const app = express()                                                 //initialize Express
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meantodo'
 const PORT = process.env.PORT || 3000                                 //set up ports
 
 app.use(express.static('client'))                                     //express base directory is the ROOT, not the folder where the server is - established in package.JSON
@@ -11,25 +13,19 @@ app.get("/api/title", (req, res) =>
   res.json({ title: "MEAN TO DO / From Node! (Now Bootstraped!)" })   //use objects here NOT STRINGS 
 )
 
-app.get("/api/messages", (req, res) =>
-  res.json({
-    messages: [
-      {
-        author: "John",
-        content: "SAAAAAAAPPP",
-      },
-      {
-        author: "Anon",
-        content: "#whodrewthepoop",
-      },
-      {
-        author: "Scott",
-        content: "noice",
-      },
-    ],  //use objects here NOT STRINGS
-     
-  })
+const Message = mongoose.model('message', {
+  author: String,
+  content: String,
+}) 
+
+app.get("/api/messages", (req, res, err) =>
+  Message
+    .find()
+    .then(messages => res.json({ messages }))
+    .catch(err)
 )
 
-
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))     //server/server.js console.log()
+mongoose.promise = promise
+mongoose.connect(MONGODB_URL, () =>
+  app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))     //server/server.js console.log()
+)
